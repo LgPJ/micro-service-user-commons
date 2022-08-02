2b4ef1fccae18dbd81d777edc2e115a600be4c14
LgPJ <guillermobarreto100@gmail.com>
2022-08-02 15:59:37 -0400

    service commons user

---
+++ src/main/java/com/microservice/commons/user/entity/Users.java
@@ -0,0 +1,103 @@
+package com.microservice.commons.user.entity;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+@Entity
+@Table(name = "users")
+public class Users implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "username",unique = true, length = 20)
+	private String userName;
+
+	@Column(name = "password",length = 60)
+	private String password;
+
+	private Boolean isActive;
+	
+	private String name;
+
+	@Column(unique = true)
+	private String email;
+	
+	@ManyToMany(fetch = FetchType.LAZY)
+	@JoinTable(name = "usser_to_role", joinColumns = @JoinColumn(name = "users_id"), 
+	inverseJoinColumns = @JoinColumn(name = "role_id"), 
+	uniqueConstraints = {@UniqueConstraint(columnNames = {"users_id", "role_id"})})
+	private List<Role> roles;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getIsActive() {
+		return isActive;
+	}
+
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(List<Role> roles) {
+		this.roles = roles;
+	}
+
+}
---
+++ src/test/java/com/microservice/commons/user/MicroServiceUserCommonsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservice.commons.user;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroServiceUserCommonsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
